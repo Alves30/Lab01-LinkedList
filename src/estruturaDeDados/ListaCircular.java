3dca1f92140a50b1af0ce1df2508040a96034915
Gabriel Alves <gabriel.alvesaraujo3003@gmail.com>
2024-12-05 02:42:00 +0000

    segundo commit

--- src/estruturaDeDados/ListaCircular.java
+++ src/estruturaDeDados/ListaCircular.java
@@ -1,15 +1,23 @@
 package estruturaDeDados;
 
 public class ListaCircular {
+    
+    public No ultimo;
 
     public ListaCircular() {
-        // TODO Auto-generated constructor stub
-        throw new UnsupportedOperationException("Unimplemented constructor 'ListaCircular'");
+        this.ultimo = null;
     }
 
     public void insereInicio(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereInicio'");
+        No novoNo = new No(i);
+
+        if (ultimo == null) {
+            ultimo = novoNo;
+            ultimo.prx = ultimo;
+        } else {
+            novoNo.prx = ultimo.prx;
+            ultimo.prx = novoNo;
+        }
     }
 
     public boolean buscaElemento(int i) {
@@ -18,18 +26,45 @@ public class ListaCircular {
     }
 
     public Object buscaIndice(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscaIndice'");
+        if (ultimo == null) {
+            return false;
+        }
+        No noAtual = ultimo.prx;
+        int cont = 0;
+        do {
+            if (cont == i) {
+                return noAtual.valor;
+            }
+            cont++;
+            noAtual = noAtual.prx;
+        } while (noAtual != ultimo.prx);
+
+        return false;
     }
 
     public void insereFim(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereFim'");
+        insereInicio(i);
+        ultimo = ultimo.prx;
     }
 
     public void removeInicio() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeInicio'");
+        if (ultimo == null) {
+            System.out.println("Lista vazia");
+            return;
+        }
+
+        if(ultimo.prx == ultimo) {
+            ultimo = null;
+        } else {
+            No noAtual = ultimo.prx;
+
+            while (noAtual.prx != ultimo) {
+                noAtual = noAtual.prx;
+            }
+
+            noAtual.prx = ultimo.prx;
+            ultimo = noAtual;
+        }
     }
 
     public void removeFim() {
